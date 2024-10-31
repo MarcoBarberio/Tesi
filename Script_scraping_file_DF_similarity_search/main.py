f89648db4528af8186501e1f7e45817bd9080e19
Marco <mbarberio23@gmail.com>
2024-10-31 16:57:35 +0100

    Aggiornamento dizionario

--- Script_scraping_file_DF_similarity_search/main.py
+++ Script_scraping_file_DF_similarity_search/main.py
@@ -3,10 +3,10 @@ from similarity_search import get_similarity
 import random
 from node import File_node,URL_node
 from datetime import datetime
-def crawl(node,max_depth):
+def crawl(node,max_depth,visited_url):
     if node is None:
         return None
-    if node.depth>max_depth:
+    if node.depth>=max_depth:
         return None
     links=get_links(node.url)
     if links["status_code"]!=200:
@@ -15,15 +15,18 @@ def crawl(node,max_depth):
     url_nodes=[]
     file_nodes=[]
     for link in links["redirect_links"]:
+        if link in visited_url:
+            continue
+        visited_url.append(link)
         resource_name=get_resource_name(link)
-        similarity=get_similarity(resource_name)
+        similarity=get_similarity(link)
         if similarity>0:
             new_node=URL_node(link,node.depth+1,node,resource_name,similarity)
             new_node.print_data()
             url_nodes.append(new_node)
     for file in links["files"]:
         resource_name=get_resource_name(file)
-        similarity=get_similarity(resource_name)
+        similarity=get_similarity(file)
         if similarity>0:
             new_node=File_node(file,node.depth+1,node,get_extension(file),resource_name,similarity)
             new_node.print_data()
@@ -38,9 +41,11 @@ def crawler(root_url,max_depth):
     root=URL_node(root_url,0,None,root_resource_name,0)
     url_queue=[root]
     file_queue=[]
+    visited_url=[root]
     while url_queue:
-        node=url_queue.pop()
-        links=crawl(node,max_depth)
+        node=url_queue.pop(0) #nodo con priorità piu alta
+        visited_url.append(node.url)
+        links=crawl(node,max_depth,visited_url)
         if links is not None:
             url_queue.extend(links[0])
             file_queue.extend(links[1])
@@ -51,7 +56,7 @@ def crawler(root_url,max_depth):
 if __name__=="__main__":
     depth=1
     x=datetime.now()
-    crawler("https://www.efrag.org/en",depth)
+    crawler("https://quotes.toscrape.com/",depth)
     diff=datetime.now()-x
     total_seconds = diff.total_seconds()
     minutes = int(total_seconds // 60)
--- Script_scraping_file_DF_similarity_search/similarity_search.py
+++ Script_scraping_file_DF_similarity_search/similarity_search.py
@@ -1,6 +1,6 @@
 import numpy
 from transformers import XLMRobertaTokenizer, XLMRobertaModel
-from sklearn.metrics.pairwise import cosine_distances
+from sklearn.metrics.pairwise import cosine_similarity
 import torch 
 from create_centroid import create_centroid
 import os
@@ -23,5 +23,5 @@ def get_similarity(word):
     word_embedding=outputs.last_hidden_state.mean(dim=1).squeeze().numpy()
     reshaped_word_embedding=word_embedding.reshape(1,-1) # con reshape si cambia la struttura del tensore in matrice a due dimensioni senza alterare i dati
     reshaped_centroid=centroid.reshape(1,-1)
-    similarity=cosine_distances(reshaped_word_embedding,reshaped_centroid)[0][0] #somiglianza tra centroide e vettore di parole calcolata come il coseno dell'angolo tra i tensori
+    similarity=cosine_similarity(reshaped_word_embedding,reshaped_centroid)[0][0] #somiglianza tra centroide e vettore di parole calcolata come il coseno dell'angolo tra i tensori
     return similarity
